01746ba0fb837bca41f0baaeed88da58a6e0ec80
Michael Rivera <mrivera.lee@gmail.com>
2012-02-23 23:46:16 -0500

    Added Sample Attendee Listing for Milan 2012

---
+++ Wharton-Event-Planning/src/edu/upenn/cis350/AttendeeActivity.java
@@ -0,0 +1,22 @@
+package edu.upenn.cis350;
+
+import android.app.Activity;
+import edu.upenn.cis350.R;  // Remember to add this to all activity files
+import android.os.Bundle;
+
+public class AttendeeActivity extends Activity {
+
+
+
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	   
+	        setContentView(R.layout.attendee);
+	}
+	    // TODO Auto-generated method stub
+
+	
+}
--- Wharton-Event-Planning/src/edu/upenn/cis350/WhartonEventMainActivity.java
+++ Wharton-Event-Planning/src/edu/upenn/cis350/WhartonEventMainActivity.java
@@ -1,13 +1,26 @@
 package edu.upenn.cis350;
-
+import edu.upenn.cis350.R;  // Remember to add this to all activity files
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WhartonEventMainActivity extends Activity {
-    /** Called when the activity is first created. */
+   
+	public static final int ACTIVITY_CreateAttendee = 1;
+	
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+       setContentView(R.layout.main);
+    }
+    
+    // Launch Activity for Attendee Listing for Milan 2012
+    public void onAttendeeButtonClick(View view){
+    	Intent i = new Intent(this, AttendeeActivity.class);
+    	startActivity(i);
+    	
     }
 }
